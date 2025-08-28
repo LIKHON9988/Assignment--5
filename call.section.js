705b09898b82f8bdd3969159c6b5395c4f5ecea0
LIKHON9988 <likhondatta343@gmail.com>
2025-08-28 13:11:47 +0600

    heart addeded

---
+++ call.section.js
@@ -0,0 +1,4 @@
+document.getElementById("call-button").addEventListener("click"),
+  function () {
+    console.log("clicked");
+  };
---
+++ index.global.js
@@ -0,0 +1,16 @@
+// ------------------------------heart section------------------>>>>>>>>>>>>
+
+const cardHearts = document.querySelectorAll(".heart-btn");
+const navHeart = document.getElementById("nav-heart-count");
+
+let heartCount = 0;
+
+cardHearts.forEach((heart) => {
+  heart.addEventListener("click", function () {
+    heartCount++;
+    navHeart.textContent = heartCount;
+    console.log("navHeart.textContent");
+  });
+});
+
+// ---------------------------------call section---------------->>>>>>>>>>>>>>
